c16c55dbcacd20cdef101fbffadd8d403026ca84
jalvess021 <jalvess021@gmail.com>
2023-05-30 19:31:43 -0300

    ft: implements store and update repository support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,67 +2,75 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support /*cria o objeto e injeta na variavel*/){
 
-        $supports = $support -> all();
-        //dd($supports);
+    public function __construct(
+        protected SupportService $service
+    ){}
+
+    public function index(Request $request){
+        $supports = $this->service->getAll($request->filter);
+        dd($supports);
         return view('admin/supports/index', compact('supports') /*ou ['supports' => $supports]*/);
-        //retorna uma variavel com todos os valores que quero listar
     }
 
-    public function show(string|int $id, Support $support){
+    public function show(string $id)
+    {
+        //support::find($id);
         //support::where('id', $id)->first();
         //support::where('id', '=', $id)->first(); Parametro de comparacao
-        if (!$support = $support -> find($id)){ //se a variavel n receber um retorno de um id do bd, retorna para a url anterior.
+        if (!$support = $this->service->findOne($id)){ //se a variavel n receber um retorno de um id do bd, retorna para a url anterior.
             return back();
         }
         return view('admin/supports/show', compact('support'));
-        dd($support);
     }
 
-    public function create(){
-        
+    public function create()
+    {    
         return view('admin/supports/create');
     }
 
-    public function edit(string|int $id, Support $support){
-        if (!$support = $support -> where('id', '=', $id)->first()){ 
+    public function edit(string $id)
+    {
+        if (!$support = $this->service->findOne($id)){ 
             return back();
         }
         return view('admin/supports.edit', compact('support'));
     }
 
-    public function store(Request $request, Support $support){
-     /*dd($request->only(['subject', '_token'])); 
-        dd($request->all()); */
-        $data = $request -> all();
-        $data['status'] = 'a';  
-        
-       $support = $support -> create($data);
-       
+    public function store(StoreUpdateSupport $request, Support $support)
+    {
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
        return redirect()-> route("supports.index");
     }
 
-    public function update(Request $request, string $id, Support $support){
+    public function update(StoreUpdateSupport $request, Support $support){
     
-        if (!$support = $support -> find($id)){ 
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+
+        if (!$support){ 
             return back();
         }
-        $support->update($request->only(['subject', 'body']));
+
         return redirect()-> route('supports.index');
     }
 
-    public function destroy(string|int $id){
-        if (!$support = Support::find($id)){ 
-            return back();
-        }
-        $support->delete();
+    public function destroy(string $id){
+        $this->service->delete($id);
+
         return redirect()->route('supports.index');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Support;
+use App\Repositories\SupportRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use SupportEloquentORM;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            SupportRepositoryInterface::class, 
+            SupportEloquentORM::class
+        );
+
+        //Onde tiver uma classe abstrata(interface), injeta ela na classe concreta.
     }
 
     /**
